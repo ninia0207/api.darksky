efcab0ca0e6218965bb8fc3de136f053b08f9b1a
ninia0207 <ninia0207@gmail.com>
2021-10-30 14:13:06 +0400

    add web client

---
+++ Weather.API/WebClient/Abstractions/IWebClient.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace WebClient.Abstractions
+{
+    public interface IWebClient
+    {
+        public Task<string> GetDataAsync();
+    }
+}
---
+++ Weather.API/WebClient/Implementations/WebClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using WebClient.Abstractions;
+
+namespace WebClient.Implementations
+{
+    public class WebClient : IWebClient
+    {
+        
+        public string URL { get; set; }
+
+        public WebClient(string url)
+        {
+            URL = url;
+        }
+
+
+        public async Task<string> GetDataAsync()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var content = await httpClient.GetStringAsync(URL);
+                return content;
+            }
+        }
+    }
+}
